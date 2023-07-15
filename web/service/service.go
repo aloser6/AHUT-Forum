abaf7291cd2c02985b2c9accadfcac49942fc6fd
aloser <2878993617@qq.com>
2023-07-15 17:13:24 +0800

    add:完善心跳机制

--- web/service/service.go
+++ web/service/service.go
@@ -3,15 +3,18 @@ package service
 import (
 	"errors"
 	"sync"
+	"time"
 )
 
+var TIMEOUT time.Duration = 75 * time.Second
+
 type Service struct {
-	Sname string //服务器名
-	Ip    string //ip
-	Port  int32  //port
-	//TODO time
-	LiveTime int64 `default:"0"`     //服务上线时间
-	isLive   bool  `default:"false"` //是否存活
+	Sname    string //服务器名
+	Ip       string //ip
+	Port     int32  //port
+	LiveTime int64  `default:"0"`     //服务上线时间
+	timeOut  int32  `default:"0"`     //服务为心跳的时间
+	isLive   bool   `default:"false"` //是否存活
 }
 
 type Centre struct {
@@ -26,7 +29,7 @@ type Centre struct {
  *参1:存储服务信息的结构体
  *参2:任意*int8的值
  **/
-func (c *Centre) RegisterLock(args Service, val *int8) error { //参数优化val
+func (c *Centre) Register(args Service, val *int8) error { //参数优化val
 	if args.Ip == "" || args.Port < 0 {
 		return errors.New("invalid args")
 	}
@@ -56,7 +59,7 @@ func (c *Centre) RegisterLock(args Service, val *int8) error { //参数优化val
  *参1:查找对应服务的信息
  *参2:用于接受返回值
  **/
-func (c *Centre) DiscoverLock(name string, val *Service) error { //读写锁
+func (c *Centre) Discover(name string, val *Service) error { //读写锁
 	if name == "" || val == nil {
 		return errors.New("invalid args")
 	}
@@ -82,12 +85,12 @@ func (c *Centre) DiscoverLock(name string, val *Service) error { //读写锁
  *参1:删除对应服务的信息
  *参2:任意*int8的值
  **/
-func (c *Centre) DeleteLock(name string, val *int8) error {
+func (c *Centre) Delete(name string, val *int8) error {
 	if name == "" {
 		return errors.New("invalid args")
 	}
 
-	//读锁
+	//写锁
 	c.Mutex.Lock()
 	defer c.Mutex.Unlock()
 	c.IsLock = true
@@ -103,6 +106,49 @@ func (c *Centre) DeleteLock(name string, val *int8) error {
 	return nil
 }
 
-//func (c *Centre) AddTimer(name string, val *int8)
+/*功能:用于服务发送心跳
+ *参1:服务名称
+ *参2:任意*int8的值
+ **/
+func (c *Centre) HertBeat(name string, val *int8) error {
+	//lcok
+	if name == "" {
+		return errors.New("invalid args")
+	}
+
+	c.Mutex.Lock()
+	defer c.Mutex.Unlock()
+	i, err := c.Index[name]
+	if err {
+		return errors.New("name not registered")
+	}
+	c.Service[i].timeOut = 0
+	c.Service[i].LiveTime++
+	return nil
+}
+
+/*功能:检查服务是否超时
+ *参1:服务名称
+ *参2:任意*int8的值
+ **/
+func (c *Centre) CheckTimeOut(name string, val *int8) error {
+	if name == "" {
+		return errors.New("invalid args")
+	}
+	for i := 0; i < len(c.Service); i++ {
+		c.Mutex.Lock()
+		if c.Service[i].isLive == true {
+			c.Service[i].timeOut++
+		}
+		c.Mutex.Unlock()
+		if c.Service[i].timeOut > int32(TIMEOUT) {
+			err := c.Delete(name, val)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
 
 //ipv6
