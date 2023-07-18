3f84ae8991d276ab8daeadead137c529eec6aa25
aloser <2878993617@qq.com>
2023-07-18 14:42:14 +0800

    fix:rpc远程调用

--- web/utils/rpc.go
+++ web/utils/rpc.go
@@ -10,15 +10,14 @@ type Rpcer struct {
 }
 
 /*功能:实现对结构体方法的注册(仅注册，未监听)
- *参1:结构体对象
- *参2:ip:port的字符串
+ *参1:要注册的服务的结构体对象
  **/
 func (r *Rpcer) Register(class interface{}) error {
 	if class == nil {
 		return errors.New("invalid args")
 	}
-
-	if err := rpc.Register(class); err != nil {
+	err := rpc.Register(class)
+	if err != nil {
 		return err
 	}
 	rpc.HandleHTTP()
@@ -26,24 +25,32 @@ func (r *Rpcer) Register(class interface{}) error {
 	return nil
 }
 
+//监听
+/* l, e := net.Listen("tcp", ":1234")
+if e != nil {
+	log.Fatal("listen error:", e)
+}
+go http.Serve(l, nil)
+*/
+
 /*功能:实现函数的远程调用
  *参1: ip:端口号 的字符串
  *参2:函数名
- *参3:返回值(使用指针/结构体指针)
- *参4:参数(变长)
+ *参3:参数
+ *参4:返回值(使用指针/结构体指针)
  **/
 func (r *Rpcer) Call(iport string, funName string, args interface{}, val interface{}) error {
 	if iport == "" || funName == "" {
 		return errors.New("invalid args")
 	}
 
-	client, err := rpc.Dial("tcp", iport)
+	client, err := rpc.DialHTTP("tcp", iport)
 	if err != nil {
-		return errors.New("rpc.Dial fail")
+		return err
 	}
 	err = client.Call(funName, args, val)
 	if err != nil {
-		return errors.New("rpc.Call fail")
+		return err
 	}
 	//defer client.Close()??
 	return nil
