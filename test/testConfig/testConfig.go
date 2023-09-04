1ea0a953e46a0ca601c39b575f85e869cefdca02
zhangchi <1586563723@qq.com>
2023-09-04 16:00:42 +0800

    测试

--- test/testConfig/testConfig.go
+++ test/testConfig/testConfig.go
@@ -1,16 +1,11 @@
 package main
 
-import (
-	"ISPS/config"
-	"fmt"
-)
-
 func main() {
-	y := config.Yaml{}
+	//y := config.Yaml{}
 	//config.InitMySQL(y)
 	//y1 := utils.Yaml{Key: "mysql.a", Value: "a"}
 	//fmt.Println(y.ReadYaml())
 	//fmt.Println(y1.ReadYaml())
 	//y1.SetYaml()
-	fmt.Print(y.ReadYamlString("mysql.dsn"))
+	//fmt.Print(y.ReadYamlString("mysql.dsn"))
 }
