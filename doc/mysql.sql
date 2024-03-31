217ac8c870df535d7d9b2caeef763efdc8e8a451
aloser <2878993617@qq.com>
2024-03-31 16:50:05 +0800

    mod:updata some table

--- doc/mysql.sql
+++ doc/mysql.sql
@@ -1,14 +1,5 @@
-CREATE TABLE `relation` (
-  `uid` int PRIMARY KEY,
-  `pid` int UNIQUE,
-  `fid` int UNIQUE,
-  `cid` bigint UNIQUE,
-  `followee` varchar(65535),
-  `following` varchar(65535)
-);
-
 CREATE TABLE `users` (
-  `uid` integer PRIMARY KEY,
+  `uid` int PRIMARY KEY AUTO_INCREMENT,
   `account` int NOT NULL,
   `password` varchar(64) NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -19,3 +10,50 @@ CREATE TABLE `users` (
   `major` varchar(128)
 );
 
+CREATE TABLE `forums` (
+  `fid` int PRIMARY KEY AUTO_INCREMENT,
+  `uid` int,
+  `forumname` varchar(255) NOT NULL,
+  `membercount` int DEFAULT 1,
+  `description` text
+);
+
+CREATE TABLE `posts` (
+  `pid` bigint PRIMARY KEY AUTO_INCREMENT,
+  `fid` int,
+  `uid` int,
+  `title` varchar(128) NOT NULL,
+  `text` text NOT NULL,
+  `browse` int DEFAULT 0,
+  `comment` int DEFAULT 0,
+  `votes` int DEFAULT 0,
+  `releasetime` timestamp NOT NULL
+);
+
+CREATE TABLE `comments` (
+  `cid` bigint PRIMARY KEY AUTO_INCREMENT,
+  `uid` int,
+  `pid` int,
+  `fathercid` bigint DEFAULT -1,
+  `votes` int DEFAULT 0,
+  `text` text NOT NULL,
+  `releasetime` timestamp NOT NULL
+);
+
+CREATE TABLE `follow` (
+  `followid` int PRIMARY KEY,
+  `uid` int,
+  `following` int NOT NULL
+);
+
+ALTER TABLE `forums` ADD FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
+
+ALTER TABLE `posts` ADD FOREIGN KEY (`fid`) REFERENCES `forums` (`fid`);
+
+ALTER TABLE `posts` ADD FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
+
+ALTER TABLE `comments` ADD FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
+
+ALTER TABLE `comments` ADD FOREIGN KEY (`pid`) REFERENCES `posts` (`pid`);
+
+ALTER TABLE `follow` ADD FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
