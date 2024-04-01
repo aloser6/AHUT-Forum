20e24bc84297234a836365aa49f66211ab4c5ca7
aloser <2878993617@qq.com>
2024-04-01 17:19:31 +0800

    add:test_config, test_log

---
+++ test/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	test_log()
+	test_config()
+}
---
+++ test/test_config.go
@@ -0,0 +1,8 @@
+package main
+
+import "AHUT-Forum/config"
+
+func test_config() {
+	conf := config.Config{}
+	conf.Init()
+}
