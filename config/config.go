b8fa857d3376a98cff93ca3942c2880b0b933176
aloser <2878993617@qq.com>
2024-04-03 16:31:14 +0800

    fix:fix some bug and modify some call

--- config/config.go
+++ config/config.go
@@ -2,12 +2,13 @@ package config
 
 import (
 	logger "AHUT-Forum/config/log"
+	"AHUT-Forum/units"
 	"os"
 
 	"gopkg.in/yaml.v2"
 )
 
-var config_path = "../config.yml"
+var config_path = units.Get_root_path() + "/config.yml"
 
 type Config struct {
 	Mysql mysql_config `yaml:"mysql"` //yml反射不能是私有字段故大写
@@ -24,15 +25,16 @@ type mysql_config struct {
 func (conf *Config) Init() {
 	logger.Logger_init()
 	conf.config_init()
+	db := Mysql{}
+	db.Mysql_init(conf)
 }
 
 func (conf *Config) config_init() {
 	data, err := os.ReadFile(config_path)
 	logger.Assert(err)
 	err = yaml.Unmarshal(data, &conf)
-	logger.Info(conf.Mysql.Username)
 	logger.Assert(err)
-	logger.Info("config → %+v\n", conf)
+	logger.Info("config → %+v", conf)
 }
 
 // config → {Mysql:{Url:127.0.0.1 Port:3306} Redis:{Host:127.0.0.1 Port:6379}}
--- config/mysql.go
+++ config/mysql.go
@@ -12,10 +12,10 @@ type Mysql struct {
 	Db *gorm.DB
 }
 
-func (mq *Mysql) Mysql_init() {
+func (mq *Mysql) Mysql_init(conf *Config) {
 	var err error
-	config := Config{}
-	url := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", config.Mysql.Username, config.Mysql.Password, config.Mysql.Ip, config.Mysql.Port, config.Mysql.Dbname)
+	url := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", conf.Mysql.Username, conf.Mysql.Password, conf.Mysql.Ip, conf.Mysql.Port, conf.Mysql.Dbname)
+	logger.Info(url)
 	mq.Db, err = gorm.Open(mysql.Open(url), &gorm.Config{})
 	logger.Assert(err)
 }
