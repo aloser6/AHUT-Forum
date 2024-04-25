a0e9b2f14a49e4e01a61f84e21ad1e99444d3be8
limaojilimao <1502292745@qq.com>
2024-04-25 12:32:27 +0800

    update dao.go

--- web/dao/dao.go
+++ web/dao/dao.go
@@ -3,7 +3,6 @@ package dao
 import (
 	"AHUT-Forum/config"
 	logger "AHUT-Forum/config/log"
-	"reflect"
 )
 
 var mysql = config.Mysql{}
@@ -11,14 +10,10 @@ var db = mysql.Db
 
 //Insert
 func Insert(tablename string, model interface{}) {
-	mysql.Mu.RLock()
-	defer mysql.Mu.RUnlock()
+	mysql.Mu.Lock()
+	defer mysql.Mu.Unlock()
 
-	if tablename == "" {
-		return
-	}
-
-	if isEmptyModel(model) {
+	if tablename == "" || model == nil {
 		return
 	}
 
@@ -28,11 +23,10 @@ func Insert(tablename string, model interface{}) {
 
 //Update
 func Update(tablename string, model interface{}) {
-	if tablename == "" {
-		return
-	}
+	mysql.Mu.Lock()
+	defer mysql.Mu.Unlock()
 
-	if isEmptyModel(model) {
+	if tablename == "" || model == nil {
 		return
 	}
 
@@ -44,11 +38,7 @@ func Update(tablename string, model interface{}) {
 func Select(tablename string, id uint, model interface{}) []interface{} {
 	mysql.Mu.RLock()
 	defer mysql.Mu.RUnlock()
-	if tablename == "" {
-		return nil
-	}
-
-	if isEmptyModel(model) {
+	if tablename == "" || model == nil {
 		return nil
 	}
 
@@ -60,39 +50,13 @@ func Select(tablename string, id uint, model interface{}) []interface{} {
 
 //Delete
 func Delete(tablename string, model interface{}, id uint) {
-	if tablename == "" {
-		return
-	}
+	mysql.Mu.Lock()
+	defer mysql.Mu.Unlock()
 
-	if isEmptyModel(model) {
+	if tablename == "" || model == nil {
 		return
 	}
 
 	err := db.Table(tablename).Delete(model, id).Error
 	logger.Assert(err)
-}
-
-// 判断结构体是否为空
-func isEmptyModel(model interface{}) bool {
-	// 使用反射获取结构体的值
-	val := reflect.ValueOf(model)
-
-	// 如果结构体是指针类型，则获取指针指向的值
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
-
-	// 遍历结构体的字段
-	for i := 0; i < val.NumField(); i++ {
-		// 获取字段的值
-		fieldValue := val.Field(i)
-
-		// 使用反射判断字段的值是否为零值
-		if reflect.DeepEqual(fieldValue.Interface(), reflect.Zero(fieldValue.Type()).Interface()) {
-			return true
-		}
-	}
-
-	// 如果所有字段都不为空，则返回false
-	return false
 }
